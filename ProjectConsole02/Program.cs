840f2ddfc83e182d7baad81acc98f8f5fc319a02
Lorenzo Windmoller <lorenzomart01@gmail.com>
2021-08-20 14:53:55 -0300

    Added remaining db operations in ProjectConsole02

--- ProjectConsole02/Program.cs
+++ ProjectConsole02/Program.cs
@@ -1,4 +1,6 @@
-﻿using ProjectConsole02.Models;
+﻿using System;
+using System.Linq;
+using ProjectConsole02.Models;
 
 namespace ProjectConsole02
 {
@@ -8,19 +10,46 @@ namespace ProjectConsole02
     {
       using(var db = new MoviesDbContext())
       {
-        db.Movies.Add(new Movie {
-          Title = "Test 01",
-          LaunchYear = 2021,
-        });
+        // db.Movies.Add(new Movie {
+        //   Title = "Test 01",
+        //   LaunchYear = 2021,
+        // });
 
-        db.Movies.Add(new Movie {
-          Title = "Test 02",
-          LaunchYear = 2021,
-          Duration = 180,
-        });
+        // db.Movies.Add(new Movie {
+        //   Title = "Test 02",
+        //   LaunchYear = 2021,
+        //   Duration = 180,
+        // });
 
-        db.SaveChanges();
-      }
+        // db.SaveChanges();
+
+        // db.Movies.OrderBy(m => m.LaunchYear).ToList().ForEach(m => 
+        //   Console.WriteLine($"{m.Title} - {m.LaunchYear}")
+        // );
+        
+        // var movie = db.Movies.Find(2);
+        
+        // if (movie != null) 
+        // {
+        //   movie.Synopsis = "Synopsis 02";
+        //   db.SaveChanges();
+        // }
+        
+        // var movie = db.Movies.Where(m => m.Title == "Test 01").FirstOrDefault();
+        
+        // if (movie != null) 
+        // {
+        //   Console.WriteLine($"{movie.Title} - {movie.LaunchYear}");
+        // }
+
+      //   var movie = db.Movies.Find(2);
+
+      //   if (movie != null) 
+      //   {
+      //     db.Movies.Remove(movie);
+      //     db.SaveChanges();
+      //   }
+      // }
     }
   }
 }
